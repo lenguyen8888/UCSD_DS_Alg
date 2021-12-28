c20d165107ff6245f9c1d9428ad4f3920b228405
Nguyen Le <ngle@microsoft.com>
2021-12-28 11:07:59 -0800

    implement 2/5 tests for week1, will continue to wrap up

--- class2/week1_basic_data_structures/1_brackets_in_code/check_brackets.java
+++ class2/week1_basic_data_structures/1_brackets_in_code/check_brackets.java
@@ -1,6 +1,6 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
 class Bracket {
@@ -30,18 +30,41 @@ class check_brackets {
         String text = reader.readLine();
 
         Stack<Bracket> opening_brackets_stack = new Stack<Bracket>();
+        int errorPos = -1;
         for (int position = 0; position < text.length(); ++position) {
             char next = text.charAt(position);
 
             if (next == '(' || next == '[' || next == '{') {
                 // Process opening bracket, write your code here
+                Bracket item = new Bracket(next, position);
+                opening_brackets_stack.push(item);
             }
 
             if (next == ')' || next == ']' || next == '}') {
                 // Process closing bracket, write your code here
+                if (opening_brackets_stack.size() > 0) {
+                    Bracket matchItem = opening_brackets_stack.pop();
+                    if (!matchItem.Match(next)) {
+                        errorPos = position;
+                        break;
+                    }
+                } else {
+                    errorPos = position;
+                    break;
+                }
+
             }
         }
 
+        if (errorPos < 0 && opening_brackets_stack.size() > 0) {
+            Bracket item = opening_brackets_stack.pop();
+            errorPos = item.position;
+        }
+
         // Printing answer, write your code here
+        if (errorPos < 0)
+            System.out.println("Success");
+        else
+            System.out.println(errorPos + 1);
     }
 }
