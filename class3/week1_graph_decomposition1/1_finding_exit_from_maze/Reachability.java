9d74d75f582f6ef8238a60e6c5cc5a620f861129
Nguyen Le <ngle@microsoft.com>
2021-12-31 17:25:03 -0800

    Passed week 1 100%:

--- class3/week1_graph_decomposition1/1_finding_exit_from_maze/Reachability.java
+++ class3/week1_graph_decomposition1/1_finding_exit_from_maze/Reachability.java
@@ -1,18 +1,49 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 public class Reachability {
-    private static int reach(ArrayList<Integer>[] adj, int x, int y) {
-        //write your code here
-        return 0;
+//    private static int reach(ArrayList<Integer>[] adj, int x, int y) {
+//        //write your code here
+//        return 0;
+//    }
+
+    private static HashMap<Integer, Integer> visited;
+    private static List<Integer> gAdj[];
+    private static List<Integer> reach;
+
+    private static void visit(int node) {
+        reach.add(node);
+        visited.put(node, 1);
     }
 
+    private static void explore(int start, List<Integer> gAdj[]) {
+        for (int node : gAdj[start]) {
+            if (!visited.containsKey(node)) {
+                visit(node);
+                explore(node, gAdj);
+            }
+        }
+    }
+
+    private static void initExplore() {
+        visited = new HashMap<>();
+        reach = new ArrayList<>();
+    }
+
+    private static int reach(ArrayList<Integer>[] adj, int x, int y) {
+        // write your code here
+        initExplore();
+        explore(x, adj);
+        return reach.contains(y) ? 1 : 0;
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int m = scanner.nextInt();
-        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
+        ArrayList<Integer>[] adj = (ArrayList<Integer>[]) new ArrayList[n];
         for (int i = 0; i < n; i++) {
             adj[i] = new ArrayList<Integer>();
         }
@@ -28,4 +59,3 @@ public class Reachability {
         System.out.println(reach(adj, x, y));
     }
 }
-
--- class3/week1_graph_decomposition1/2_adding_exits_to_maze/ConnectedComponents.java
+++ class3/week1_graph_decomposition1/2_adding_exits_to_maze/ConnectedComponents.java
@@ -1,18 +1,59 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 public class ConnectedComponents {
+    private static HashMap<Integer, Integer> visited;
+    private static List<Integer> gAdj[];
+    private static List<Integer> reach;
+    private static int connCount;
+    private static int nVertices;
+
+    private static void visit(int node) {
+        reach.add(node);
+        visited.put(node, 1);
+    }
+
+    private static void explore(int start, List<Integer> gAdj[]) {
+        for (int node : gAdj[start]) {
+            if (!visited.containsKey(node)) {
+                visit(node);
+                explore(node, gAdj);
+            }
+        }
+    }
+
+    private static void initExplore() {
+        visited = new HashMap<>();
+        reach = new ArrayList<>();
+        connCount = 0;
+    }
+
+//    private static int numberOfComponents(ArrayList<Integer>[] adj) {
+//        int result = 0;
+//        //write your code here
+//        return result;
+//    }
+
     private static int numberOfComponents(ArrayList<Integer>[] adj) {
         int result = 0;
-        //write your code here
-        return result;
+        // write your code here
+        initExplore();
+        for (int i = 0; i < nVertices; ++i) {
+            if (!visited.containsKey(i)) {
+                ++connCount;
+                explore(i, adj);
+            }
+        }
+        return connCount;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int m = scanner.nextInt();
-        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
+        ArrayList<Integer>[] adj = (ArrayList<Integer>[]) new ArrayList[n];
         for (int i = 0; i < n; i++) {
             adj[i] = new ArrayList<Integer>();
         }
@@ -23,7 +64,7 @@ public class ConnectedComponents {
             adj[x - 1].add(y - 1);
             adj[y - 1].add(x - 1);
         }
+        nVertices = n;
         System.out.println(numberOfComponents(adj));
     }
 }
-
