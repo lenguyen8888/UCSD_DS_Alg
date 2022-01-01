e8f673ca5116c908011cbc7de45381c48f763eb8
Nguyen Le <ngle@microsoft.com>
2022-01-01 15:44:55 -0800

    Pass week 3 100%, Algo Toolbox/passed week1 100% in C++

--- 3_Algorithm_On_Graphs/week3_paths_in_graphs1/1_flight_segments/BFS.java
+++ 3_Algorithm_On_Graphs/week3_paths_in_graphs1/1_flight_segments/BFS.java
@@ -1,19 +1,44 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class BFS {
+//    private static int distance(ArrayList<Integer>[] adj, int s, int t) {
+//        //write your code here
+//        return -1;
+//    }
+    private static int nVertices;
+
     private static int distance(ArrayList<Integer>[] adj, int s, int t) {
-        //write your code here
-        return -1;
+        LinkedList<Integer> bfsQ = new LinkedList<>();
+        int dist[] = new int[nVertices];
+        int prev[] = new int[nVertices];
+        for (int i = 0; i < nVertices; ++i) {
+            dist[i] = -1;
+            prev[i] = -1;
+        }
+        bfsQ.add(s);
+        dist[s] = 0;
+        prev[s] = -1;
+        while (!bfsQ.isEmpty()) {
+            int parent = bfsQ.pop();
+            for (int node : adj[parent]) {
+                if (dist[node] < 0) {
+                    dist[node] = dist[parent] + 1;
+                    prev[node] = parent;
+                    bfsQ.add(node);
+                }
+            }
+        }
+        return dist[t];
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
+        nVertices = n;
         int m = scanner.nextInt();
-        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
+        ArrayList<Integer>[] adj = (ArrayList<Integer>[]) new ArrayList[n];
         for (int i = 0; i < n; i++) {
             adj[i] = new ArrayList<Integer>();
         }
@@ -29,4 +54,3 @@ public class BFS {
         System.out.println(distance(adj, x, y));
     }
 }
-
--- 3_Algorithm_On_Graphs/week3_paths_in_graphs1/2_bipartite/Bipartite.java
+++ 3_Algorithm_On_Graphs/week3_paths_in_graphs1/2_bipartite/Bipartite.java
@@ -1,19 +1,47 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class Bipartite {
+    private static int nVertices;
+
+//    private static int bipartite(ArrayList<Integer>[] adj) {
+//        //write your code here
+//        return -1;
+//    }
     private static int bipartite(ArrayList<Integer>[] adj) {
-        //write your code here
-        return -1;
+        LinkedList<Integer> bfsQ = new LinkedList<>();
+        int dist[] = new int[nVertices];
+        for (int i = 0; i < nVertices; ++i) {
+            dist[i] = -1;
+        }
+        for (int start = 0; start < nVertices; ++start) {
+            if (dist[start] < 0) {
+                bfsQ.add(start);
+                dist[start] = 0;
+            }
+            while (!bfsQ.isEmpty()) {
+                int parent = bfsQ.pop();
+                for (int node : adj[parent]) {
+                    if (dist[node] < 0) {
+                        dist[node] = dist[parent] + 1;
+                        bfsQ.add(node);
+                    } else {
+                        if (dist[parent] == dist[node])
+                            return 0;
+                    }
+                }
+            }
+        }
+        return 1;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
+        nVertices = n;
         int m = scanner.nextInt();
-        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
+        ArrayList<Integer>[] adj = (ArrayList<Integer>[]) new ArrayList[n];
         for (int i = 0; i < n; i++) {
             adj[i] = new ArrayList<Integer>();
         }
@@ -27,4 +55,3 @@ public class Bipartite {
         System.out.println(bipartite(adj));
     }
 }
-
