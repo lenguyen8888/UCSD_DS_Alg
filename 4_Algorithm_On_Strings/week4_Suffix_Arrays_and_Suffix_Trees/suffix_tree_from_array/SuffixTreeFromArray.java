50fdb3a2238941d5657f81a19030f43973d0ceb5
Nguyen Le <ngle@microsoft.com>
2022-01-08 09:21:43 -0800

    Finally passed class4 week4 100%, very neat stack data structure to avoid recursion

--- 4_Algorithm_On_Strings/week4_Suffix_Arrays_and_Suffix_Trees/suffix_tree_from_array/SuffixTreeFromArray.java
+++ 4_Algorithm_On_Strings/week4_Suffix_Arrays_and_Suffix_Trees/suffix_tree_from_array/SuffixTreeFromArray.java
@@ -1,9 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class SuffixTreeFromArray {
@@ -44,30 +47,15 @@ public class SuffixTreeFromArray {
         }
     }
 
-    // Build suffix tree of the string text given its suffix array suffix_array
-    // and LCP array lcp_array. Return the tree as a mapping from a node ID
-    // to the list of all outgoing edges of the corresponding node. The edges in the
-    // list must be sorted in the ascending order by the first character of the edge
-    // label.
-    // Root must have node ID = 0, and all other node IDs must be different
-    // nonnegative integers.
-    //
-    // For example, if text = "ACACAA$", an edge with label "$" from root to a node
-    // with ID 1
-    // must be represented by new Edge(1, 6, 7). This edge must be present in the
-    // list tree.get(0)
-    // (corresponding to the root node), and it should be the first edge in the list
-    // (because it has the smallest first character of all edges outgoing from the
-    // root).
-//    Map<Integer, List<Edge>> SuffixTreeFromSuffixArray(
-//            int[] suffixArray,
-//            int[] lcpArray,
-//            final String text) {
-//        Map<Integer, List<Edge>> tree = new HashMap<Integer, List<Edge>>();
-//        // Implement this function yourself
-//        return tree;
-//    }
     private final static char[] ALPHABET = "$ACGT".toCharArray();
+
+    private static char[] reverse(char[] a) {
+        String aStr = new String(a);
+        String revStr = new StringBuilder(aStr).reverse().toString();
+        return revStr.toCharArray();
+    }
+
+    private final static char[] REV_ALPHABET = reverse(ALPHABET);
 
     private class SuffixTree {
         private final String text; // Make sure we only keep 1 copy to avoid stack overlow!
@@ -148,8 +136,68 @@ public class SuffixTreeFromArray {
             return midNode;
         }
 
-        private static final boolean DEBUG = false;
+        private static final boolean DEBUG = true;
 
+    }
+
+//    private void dfsEdge(SuffixTree.Node node) {
+//        if (node.parent != null) {
+//            System.out.println(node.start + " " + node.end);
+//        }
+//        for (char c : ALPHABET) {
+//            if (node.children.containsKey(c)) {
+//                SuffixTree.Node child = node.children.get(c);
+//                dfsEdge(child);
+//            }
+//        }
+//    }
+
+    // Build suffix tree of the string text given its suffix array suffix_array
+    // and LCP array lcp_array. Return the tree as a mapping from a node ID
+    // to the list of all outgoing edges of the corresponding node. The edges in the
+    // list must be sorted in the ascending order by the first character of the edge
+    // label.
+    // Root must have node ID = 0, and all other node IDs must be different
+    // nonnegative integers.
+    //
+    // For example, if text = "ACACAA$", an edge with label "$" from root to a node
+    // with ID 1
+    // must be represented by new Edge(1, 6, 7). This edge must be present in the
+    // list tree.get(0)
+    // (corresponding to the root node), and it should be the first edge in the list
+    // (because it has the smallest first character of all edges outgoing from the
+    // root).
+    Map<Integer, List<Edge>> SuffixTreeFromSuffixArray(int[] suffixArray, int[] lcpArray, final String text) {
+
+        Map<Integer, List<Edge>> tree = new HashMap<Integer, List<Edge>>();
+
+        SuffixTree sTree = new SuffixTree(text, suffixArray, lcpArray);
+        SuffixTree.Node root = sTree.stFromSA();
+        int nodeCount = -1;
+
+        ArrayDeque<SuffixTree.Node> queue = new ArrayDeque<>();
+        ArrayDeque<SuffixTree.Node> debugQ = new ArrayDeque<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            SuffixTree.Node node = queue.pop();
+            if (SuffixTree.DEBUG)
+                System.out.println(">> " + node.start + "," + node.end);
+            tree.put(++nodeCount, new ArrayList<>());
+            int curNodeID = nodeCount;
+            List<Edge> curList = tree.get(curNodeID);
+            if (node.parent != null) {
+                curList.add(new Edge(++nodeCount, node.start, node.end));
+            }
+            for (char c : ALPHABET) {
+                if (node.children.containsKey(c)) {
+                    SuffixTree.Node child = node.children.get(c);
+                    curList.add(new Edge(++nodeCount, child.start, child.end));
+                    if (child.children.size() > 0)
+                        queue.add(child);
+                }
+            }
+        }
+        return tree;
     }
 
     private void dfsEdge(SuffixTree.Node node) {
@@ -164,14 +212,27 @@ public class SuffixTreeFromArray {
         }
     }
 
-    // Change the contract to collect just edge list.
-    // The template code data structure is too confusing!
-    ArrayList<String> SuffixTreeFromSuffixArray(int[] suffixArray, int[] lcpArray, final String text) {
-        ArrayList<String> edgeList = new ArrayList<>();
-        SuffixTree tree = new SuffixTree(text, suffixArray, lcpArray);
-        SuffixTree.Node root = tree.stFromSA();
-        dfsEdge(root);
-        return edgeList;
+    private void iterative_dfsEdge(SuffixTree.Node node) {
+        Stack<SuffixTree.Node> stack = new Stack<>();
+        stack.push(node);
+        while (!stack.isEmpty()) {
+            SuffixTree.Node cur = stack.pop();
+            if (cur.parent != null) {
+                System.out.println(cur.start + " " + cur.end);
+            }
+
+            // use heap/data stack would be a great start to avoid
+            // system/code stack overflow
+            //
+            // to preserve calling order we need to iterate the
+            // children list in reverse
+            for (char c : REV_ALPHABET) {
+                if (cur.children.containsKey(c)) {
+                    SuffixTree.Node child = cur.children.get(c);
+                    stack.push(child);
+                }
+            }
+        }
     }
 
     static public void main(String[] args) throws IOException {
@@ -183,6 +244,8 @@ public class SuffixTreeFromArray {
             System.out.println(a);
         }
     }
+
+    final static private boolean USE_SIMPLE_PRINT = true;
 
     public void run() throws IOException {
         FastScanner scanner = new FastScanner();
@@ -196,57 +259,77 @@ public class SuffixTreeFromArray {
             lcpArray[i] = scanner.nextInt();
         }
         System.out.println(text);
+        if (USE_SIMPLE_PRINT) {
+            simplePrint(text, suffixArray, lcpArray);
+        } else {
+            templatePrint(text, suffixArray, lcpArray);
+        }
+    }
+
+    /**
+     * @param text
+     * @param suffixArray
+     * @param lcpArray
+     */
+    private void simplePrint(String text, int[] suffixArray, int[] lcpArray) {
         SuffixTree tree = new SuffixTree(text, suffixArray, lcpArray);
         SuffixTree.Node root = tree.stFromSA();
-        dfsEdge(root);
+        iterative_dfsEdge(root);
+    }
+
+    /**
+     * @param text
+     * @param suffixArray
+     * @param lcpArray
+     */
+    private void templatePrint(String text, int[] suffixArray, int[] lcpArray) {
         // Build the suffix tree and get a mapping from
         // suffix tree node ID to the list of outgoing Edges.
-//        Map<Integer, List<Edge>> suffixTree = SuffixTreeFromSuffixArray(suffixArray, lcpArray, text);
-//        ArrayList<String> result = new ArrayList<String>();
-//        // Output the edges of the suffix tree in the required order.
-//        // Note that we use here the contract that the root of the tree
-//        // will have node ID = 0 and that each vector of outgoing edges
-//        // will be sorted by the first character of the corresponding edge label.
-//        //
-//        // The following code avoids recursion to avoid stack overflow issues.
-//        // It uses two stacks to convert recursive function to a while loop.
-//        // This code is an equivalent of
-//        //
-//        // OutputEdges(tree, 0);
-//        //
-//        // for the following _recursive_ function OutputEdges:
-//        //
-//        // public void OutputEdges(Map<Integer, List<Edge>> tree, int nodeId) {
-//        // List<Edge> edges = tree.get(nodeId);
-//        // for (Edge edge : edges) {
-//        // System.out.println(edge.start + " " + edge.end);
-//        // OutputEdges(tree, edge.node);
-//        // }
-//        // }
-//        //
-//        int[] nodeStack = new int[text.length()];
-//        int[] edgeIndexStack = new int[text.length()];
-//        nodeStack[0] = 0;
-//        edgeIndexStack[0] = 0;
-//        int stackSize = 1;
-//        while (stackSize > 0) {
-//            int node = nodeStack[stackSize - 1];
-//            int edgeIndex = edgeIndexStack[stackSize - 1];
-//            stackSize -= 1;
-//            if (suffixTree.get(node) == null) {
-//                continue;
-//            }
-//            if (edgeIndex + 1 < suffixTree.get(node).size()) {
-//                nodeStack[stackSize] = node;
-//                edgeIndexStack[stackSize] = edgeIndex + 1;
-//                stackSize += 1;
-//            }
-//            result.add(suffixTree.get(node).get(edgeIndex).start + " " + suffixTree.get(node).get(edgeIndex).end);
-//            nodeStack[stackSize] = suffixTree.get(node).get(edgeIndex).node;
-//            edgeIndexStack[stackSize] = 0;
-//            stackSize += 1;
-//        }
-//        ArrayList<String> result = SuffixTreeFromSuffixArray(suffixArray, lcpArray, text);
-//        print(result);
+        Map<Integer, List<Edge>> suffixTree = SuffixTreeFromSuffixArray(suffixArray, lcpArray, text);
+        ArrayList<String> result = new ArrayList<String>();
+        // Output the edges of the suffix tree in the required order.
+        // Note that we use here the contract that the root of the tree
+        // will have node ID = 0 and that each vector of outgoing edges
+        // will be sorted by the first character of the corresponding edge label.
+        //
+        // The following code avoids recursion to avoid stack overflow issues.
+        // It uses two stacks to convert recursive function to a while loop.
+        // This code is an equivalent of
+        //
+        // OutputEdges(tree, 0);
+        //
+        // for the following _recursive_ function OutputEdges:
+        //
+        // public void OutputEdges(Map<Integer, List<Edge>> tree, int nodeId) {
+        // List<Edge> edges = tree.get(nodeId);
+        // for (Edge edge : edges) {
+        // System.out.println(edge.start + " " + edge.end);
+        // OutputEdges(tree, edge.node);
+        // }
+        // }
+        //
+        int[] nodeStack = new int[text.length()];
+        int[] edgeIndexStack = new int[text.length()];
+        nodeStack[0] = 0;
+        edgeIndexStack[0] = 0;
+        int stackSize = 1;
+        while (stackSize > 0) {
+            int node = nodeStack[stackSize - 1];
+            int edgeIndex = edgeIndexStack[stackSize - 1];
+            stackSize -= 1;
+            if (suffixTree.get(node) == null) {
+                continue;
+            }
+            if (edgeIndex + 1 < suffixTree.get(node).size()) {
+                nodeStack[stackSize] = node;
+                edgeIndexStack[stackSize] = edgeIndex + 1;
+                stackSize += 1;
+            }
+            result.add(suffixTree.get(node).get(edgeIndex).start + " " + suffixTree.get(node).get(edgeIndex).end);
+            nodeStack[stackSize] = suffixTree.get(node).get(edgeIndex).node;
+            edgeIndexStack[stackSize] = 0;
+            stackSize += 1;
+        }
+        print(result);
     }
 }
