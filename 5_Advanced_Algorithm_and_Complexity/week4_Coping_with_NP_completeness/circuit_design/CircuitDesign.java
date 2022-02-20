6e8705cbc21dba79a234498d1925991088a4b6cf
Nguyen Le <ngle@microsoft.com>
2022-02-20 09:01:27 -0800

    Port MergingTables from C++ starter file

--- 5_Advanced_Algorithm_and_Complexity/week4_Coping_with_NP_completeness/circuit_design/CircuitDesign.java
+++ 5_Advanced_Algorithm_and_Complexity/week4_Coping_with_NP_completeness/circuit_design/CircuitDesign.java
@@ -5,92 +5,293 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Deque;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Locale;
+import java.util.Set;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class CircuitDesign {
 
-    static class StronglyConnected {
-        private static int[] visited;
-        private static int nVertices;
+    /**
+     * Adapted from Robert Sedgewick Digraph class implementation
+     * 
+     * The {@code Digraph} class represents a directed graph of vertices named 0
+     * through <em>V</em> - 1. It supports the following two primary operations: add
+     * an edge to the digraph, iterate over all of the vertices adjacent from a
+     * given vertex. It also provides methods for returning the indegree or
+     * outdegree of a vertex, the number of vertices <em>V</em> in the digraph, the
+     * number of edges <em>E</em> in the digraph, and the reverse digraph. Parallel
+     * edges and self-loops are permitted.
+     * <p>
+     * This implementation uses an <em>adjacency-lists representation</em>, which is
+     * a vertex-indexed array of {@link Bag} objects. It uses &Theta;(<em>E</em> +
+     * <em>V</em>) space, where <em>E</em> is the number of edges and <em>V</em> is
+     * the number of vertices. The <code>reverse()</code> method takes
+     * &Theta;(<em>E</em> + <em>V</em>) time and space; all other instancce methods
+     * take &Theta;(1) time. (Though, iterating over the vertices returned by
+     * {@link #adj(int)} takes time proportional to the outdegree of the vertex.)
+     * Constructing an empty digraph with <em>V</em> vertices takes
+     * &Theta;(<em>V</em>) time; constructing a digraph with <em>E</em> edges and
+     * <em>V</em> vertices takes &Theta;(<em>E</em> + <em>V</em>) time.
+     * <p>
+     * For additional documentation, see
+     * <a href="https://algs4.cs.princeton.edu/42digraph">Section 4.2</a> of
+     * <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+     *
+     * @author Robert Sedgewick
+     * @author Kevin Wayne
+     */
+    class Digraph {
+        private int V;
+        private int E;
+        private Set<Integer>[] adj;
+        private int[] indegree;
 
-        private static void initDfs() {
-            visited = new int[nVertices];
-            Arrays.fill(visited, 0);
-        }
-
-        private final static int DFS_START = 1;
-        private final static int DFS_DONE = 2;
-
-        private static void visit(int node, int color) {
-            visited[node] = color;
-        }
-
-        private static boolean hasVisited(int node) {
-            return visited[node] != 0;
-        }
-
-        private static Deque<Integer> toposort(ArrayList<Integer>[] adj) {
-            Deque<Integer> order = new ArrayDeque<Integer>();
-            initDfs();
-            for (int i = 0; i < nVertices; ++i) {
-                if (!hasVisited(i))
-                    dfs(adj, order, i);
+        Digraph(int V) {
+            this.V = V;
+            adj = (Set<Integer>[]) new HashSet[V];
+            for (int i = 0; i < adj.length; ++i) {
+                adj[i] = new HashSet<>();
             }
-            return order;
+            indegree = new int[V];
         }
 
-        private static void dfs(ArrayList<Integer>[] adj, Deque<Integer> order, int s) {
-            visit(s, DFS_START);
-            for (int node : adj[s]) {
-                if (visited[node] == 0) {
-                    dfs(adj, order, node);
-                } else if (visited[node] == DFS_START) {
-                    assert (false);
+        /**
+         * Initializes a new digraph that is a deep copy of the specified digraph.
+         *
+         * @param G the digraph to copy
+         * @throws IllegalArgumentException if {@code G} is {@code null}
+         */
+        public Digraph(Digraph G) {
+            if (G == null)
+                throw new IllegalArgumentException("argument is null");
+
+            this.V = G.V();
+            this.E = G.E();
+            if (V < 0)
+                throw new IllegalArgumentException("Number of vertices in a Digraph must be non-negative");
+
+            // update indegrees
+            indegree = new int[V];
+            for (int v = 0; v < V; v++)
+                this.indegree[v] = G.indegree(v);
+
+            // update adjacency lists
+            adj = (Set<Integer>[]) new HashSet[V];
+            for (int v = 0; v < V; v++) {
+                adj[v] = new HashSet<Integer>();
+            }
+
+            for (int v = 0; v < G.V(); v++) {
+                // reverse so that adjacency list is in same order as original
+                Stack<Integer> reverse = new Stack<Integer>();
+                for (int w : G.adj[v]) {
+                    reverse.push(w);
+                }
+                for (int w : reverse) {
+                    adj[v].add(w);
                 }
             }
-            order.addFirst(s);
-            visit(s, DFS_DONE);
         }
 
-        private static ArrayList<Integer>[] reverseGraph(ArrayList<Integer>[] adj) {
-            ArrayList<Integer>[] revAdj = (ArrayList<Integer>[]) new ArrayList[nVertices];
-            for (int i = 0; i < nVertices; ++i) {
-                revAdj[i] = new ArrayList<Integer>();
-            }
-            for (int i = 0; i < nVertices; ++i) {
-                for (int j : adj[i]) {
-                    revAdj[j].add(i);
-                }
-            }
-            return revAdj;
+        int V() {
+            return V;
         }
 
-        private static int findSCCs(ArrayList<Integer>[] adj, List<Deque<Integer>> components, int[] sccNumber) {
-            // write your code here
-            ArrayList<Integer>[] revAdj = reverseGraph(adj);
-            Deque<Integer> order = toposort(revAdj);
+        int E() {
+            return E;
+        }
 
-            initDfs();
-            Deque<Integer> newOrder = new ArrayDeque<>();
-            int numSCC = 0;
-            for (int node : order) {
-                if (visited[node] == 0) {
-                    Deque<Integer> scc = new ArrayDeque<>();
-                    dfs(adj, scc, node);
-                    components.add(scc);
-                    for (int i : scc) {
-                        newOrder.addFirst(i);
-                        sccNumber[i] = numSCC;
-                    }
-                    numSCC++;
+        /**
+         * Returns the vertices adjacent from vertex {@code v} in this digraph.
+         *
+         * @param v the vertex
+         * @return the vertices adjacent from vertex {@code v} in this digraph, as an
+         *         iterable
+         * @throws IllegalArgumentException unless {@code 0 <= v < V}
+         */
+        public Iterable<Integer> adj(int v) {
+            validateVertex(v);
+            return adj[v];
+        }
+
+        /**
+         * Returns the number of directed edges incident from vertex {@code v}. This is
+         * known as the <em>outdegree</em> of vertex {@code v}.
+         *
+         * @param v the vertex
+         * @return the outdegree of vertex {@code v}
+         * @throws IllegalArgumentException unless {@code 0 <= v < V}
+         */
+        public int outdegree(int v) {
+            validateVertex(v);
+            return adj[v].size();
+        }
+
+        /**
+         * Returns the number of directed edges incident to vertex {@code v}. This is
+         * known as the <em>indegree</em> of vertex {@code v}.
+         *
+         * @param v the vertex
+         * @return the indegree of vertex {@code v}
+         * @throws IllegalArgumentException unless {@code 0 <= v < V}
+         */
+        public int indegree(int v) {
+            validateVertex(v);
+            return indegree[v];
+        }
+
+        /**
+         * Returns the reverse of the digraph.
+         *
+         * @return the reverse of the digraph
+         */
+        public Digraph reverse() {
+            Digraph reverse = new Digraph(V);
+            for (int v = 0; v < V; v++) {
+                for (int w : adj(v)) {
+                    reverse.addEdge(w, v);
                 }
             }
-            return numSCC;
+            return reverse;
+        }
+
+        // throw an IllegalArgumentException unless {@code 0 <= v < V}
+        private void validateVertex(int v) {
+            if (v < 0 || v >= V)
+                throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V - 1));
+        }
+
+        /**
+         * Adds the directed edge v→w to this digraph.
+         *
+         * @param v the tail vertex
+         * @param w the head vertex
+         * @throws IllegalArgumentException unless both {@code 0 <= v < V} and
+         *                                  {@code 0 <= w < V}
+         */
+        public void addEdge(int v, int w) {
+            validateVertex(v);
+            validateVertex(w);
+            adj[v].add(w);
+            indegree[w]++;
+            E++;
+        }
+    }
+
+    class DigraphDFS {
+        private int clock;
+        private boolean[] visited;
+        private int[] post;
+        private Digraph graph;
+        private Deque<Integer> postQueue;
+
+        class IntPair {
+            int node;
+            int clkVal;
+
+            IntPair(int node, int clkVal) {
+                this.node = node;
+                this.clkVal = clkVal;
+            }
+        }
+
+        DigraphDFS(Digraph graph) {
+            this.graph = new Digraph(graph);
+            initDFS();
+            DFS();
+        }
+
+        /**
+         * 
+         */
+        private void initDFS() {
+            clock = 1;
+            final int V = this.graph.V();
+            visited = new boolean[V];
+            post = new int[V];
+            postQueue = new ArrayDeque<>();
+        }
+
+        private void dfsHelper(int node) {
+            visited[node] = true;
+            for (int v : graph.adj(node)) {
+                if (!visited[v])
+                    dfsHelper(v);
+            }
+            post[node] = clock;
+            clock++;
+        }
+
+        private void DFS() {
+            for (int node = 0; node < graph.V(); ++node)
+                dfsHelper(node);
+            IntPair[] postArray = new IntPair[graph.V()];
+            Arrays.sort(postArray, (IntPair a, IntPair b) -> {
+                return Integer.signum(a.clkVal - b.clkVal);
+            });
+            postQueue.clear();
+            for (IntPair val : postArray)
+                postQueue.add(val.node);
+        }
+
+        public Iterable<Integer> postOrder() {
+            return postQueue;
+        }
+
+        public Iterable<Integer> revPostOrder() {
+            Stack<Integer> stack = new Stack<>();
+            for (int v : postQueue)
+                stack.push(v);
+            return stack;
+        }
+
+    }
+
+    class DigraphSCC {
+        private Digraph graph;
+        private boolean[] visited;
+
+        int[] SCC_number;
+        Deque<Set<Integer>> SCC;
+
+        DigraphSCC(Digraph graph) {
+            this.graph = new Digraph(graph);
+            SCC_number = new int[graph.V()];
+            SCC = new ArrayDeque<Set<Integer>>();
+            visited = new boolean[graph.V()];
+
+            findSCC();
+        }
+
+        /**
+         * 
+         */
+        void findSCC() {
+            DigraphDFS revDFS = new DigraphDFS(this.graph.reverse());
+            int u = 1;
+            for (int v : revDFS.revPostOrder()) {
+                if (!visited[v]) {
+                    Set<Integer> sccSet = new HashSet<Integer>();
+                    Explore(v, sccSet, u);
+                    SCC.add(sccSet);
+                    u++;
+                }
+            }
+        }
+
+        void Explore(int i, Set<Integer> sccSet, int u) {
+            visited[i] = true;
+            sccSet.add(i);
+            SCC_number[i] = u;
+            for (int v : graph.adj(i)) {
+                if (!visited[v])
+                    Explore(v, sccSet, u);
+            }
+
         }
     }
 
@@ -114,23 +315,9 @@ public class CircuitDesign {
         int secondVar;
     }
 
-    class CCompPair {
-        Deque<Integer> comp;
-        int ccNum;
-
-        CCompPair(Deque<Integer> comp, int ccNum) {
-            this.comp = comp;
-            this.ccNum = ccNum;
-        }
-    }
-
     class TwoSatisfiability {
         int numVars;
         Clause[] clauses;
-
-        final private static boolean USE_NEW_CODE = true;
-        final private static boolean TEST_VAR_MAPPING = true;
-        final private static boolean USE_2X_P2_MAP = true;
 
         TwoSatisfiability(int n, int m) {
             numVars = n;
@@ -141,164 +328,6 @@ public class CircuitDesign {
         }
 
         boolean isSatisfiable(int[] result) {
-            if (!USE_NEW_CODE)
-                return origIsSatisfiable(result);
-
-            final int nVertices = USE_2X_P2_MAP ? 2 * numVars + 2 : 2 * numVars;
-            Deque<Integer> testVar = new ArrayDeque<>();
-            testVarToNodeMapping(testVar);
-            StronglyConnected.nVertices = nVertices;
-            ArrayList<Integer>[] edges = (ArrayList<Integer>[]) new ArrayList[nVertices];
-            for (int i = 0; i < edges.length; ++i)
-                edges[i] = new ArrayList<>();
-            for (Clause clause : clauses) {
-                edges[varToNode(-clause.firstVar)].add(varToNode(clause.secondVar));
-                edges[varToNode(-clause.secondVar)].add(varToNode(clause.firstVar));
-            }
-
-            List<Deque<Integer>> SCComps = new ArrayList<Deque<Integer>>();
-            int[] sccNumber = new int[nVertices];
-            int numSCC = StronglyConnected.findSCCs(edges, SCComps, sccNumber);
-            for (int i = 1; i <= numVars; ++i) {
-                if (sccNumber[varToNode(i)] == sccNumber[varToNode(-i)])
-                    return false;
-            }
-
-            ArrayList<Integer>[] ccEdges = (ArrayList<Integer>[]) new ArrayList[numSCC];
-            buildCCGraph(nVertices, edges, sccNumber, ccEdges);
-
-            List<Deque<Integer>> revCComps = new ArrayList<Deque<Integer>>();
-
-            reverseSortCCGraph(SCComps, numSCC, ccEdges, revCComps);
-
-            boolean[] assigned = new boolean[nVertices];
-            if (USE_2X_P2_MAP) {
-                assigned[0] = assigned[1] = true;
-            }
-            int[] solution = new int[nVertices];
-
-            for (Deque<Integer> scc : revCComps) {
-                for (int node : scc) {
-                    if (!assigned[node]) {
-                        assigned[node] = true;
-                        assigned[node ^ 1] = true;
-                        solution[node] = 1;
-                        solution[node ^ 1] = 0;
-                    }
-                }
-            }
-            for (int i = 1; i <= result.length; ++i)
-                result[i - 1] = solution[varToNode(i)];
-            return true;
-        }
-
-        /**
-         * @param SCComps
-         * @param numSCC
-         * @param ccEdges
-         * @param revCComps
-         */
-        private void reverseSortCCGraph(List<Deque<Integer>> SCComps, int numSCC, ArrayList<Integer>[] ccEdges,
-                List<Deque<Integer>> revCComps) {
-            StronglyConnected.nVertices = numSCC;
-            Deque<Integer> post_order = StronglyConnected.toposort(ccEdges);
-            Deque<Integer>[] compArray = (Deque<Integer>[]) new Deque[numSCC];
-            int compIndex = 0;
-            for (Deque<Integer> scc : SCComps)
-                compArray[compIndex++] = scc;
-            for (int i : post_order) {
-                revCComps.add(compArray[i]);
-            }
-        }
-
-        /**
-         * @param nVertices
-         * @param edges
-         * @param sccNumber
-         * @param ccEdges
-         */
-        private void buildCCGraph(final int nVertices, ArrayList<Integer>[] edges, int[] sccNumber,
-                ArrayList<Integer>[] ccEdges) {
-            for (int i = 0; i < ccEdges.length; ++i)
-                ccEdges[i] = new ArrayList<>();
-            for (int i = 0; i < nVertices; ++i) {
-                for (int j : edges[i]) {
-                    int u = sccNumber[i];
-                    int v = sccNumber[j];
-                    if (u != v) {
-                        ccEdges[u].add(v);
-                    }
-                }
-            }
-        }
-
-        /**
-         * @param testVar
-         */
-        private void testVarToNodeMapping(Deque<Integer> testVar) {
-            if (!TEST_VAR_MAPPING)
-                return;
-            for (int i = 1; i <= numVars; ++i) {
-                testVar.add(varToNode(i));
-                testVar.add(varToNode(-i));
-            }
-            Deque<Integer> revTest = new ArrayDeque<>();
-            for (int i : testVar) {
-                revTest.add(nodeToVar(i));
-            }
-        }
-
-        private int nodeToVar(int node) {
-            if (node < 0) {
-                throw new IllegalArgumentException("node must be >= 0 " + node);
-            }
-            if (USE_2X_P2_MAP) {
-                if ((node % 2) == 0)
-                    return (node / 2);
-                else
-                    return -(node / 2);
-            } else {
-                if ((node % 2) == 0)
-                    return (node / 2) + 1;
-                else
-                    return -((node - 1) / 2 + 1);
-            }
-        }
-
-        private int varToNode(int i) {
-            // mapping node..1 -> 0
-            // ............ -1 -> 1
-            // ............. 2 -> 2
-            // ............ -2 -> 3
-            // f(i) = i > 0 ? 2 * (i-1) :
-            // ...... i < 0 ? 2 * (-i - 1) + 1
-            // => f(i) = f(-i) ^ 1;
-
-            int nodeNum;
-            if (i == 0) {
-                throw new IllegalArgumentException("node must be != 0 " + i);
-            }
-            if (USE_2X_P2_MAP) {
-                if (i > 0) {
-                    nodeNum = 2 * i;
-                } else {
-                    nodeNum = 2 * (-i) ^ 1;
-                }
-            } else {
-                if (i > 0) {
-                    nodeNum = 2 * (i - 1);
-                } else {
-                    nodeNum = 2 * (-i - 1) ^ 1;
-                }
-            }
-            return nodeNum;
-        }
-
-        /**
-         * @param result
-         * @return
-         */
-        private boolean origIsSatisfiable(int[] result) {
             // This solution tries all possible 2^n variable assignments.
             // It is too slow to pass the problem.
             // Implement a more efficient algorithm here.
@@ -329,7 +358,6 @@ public class CircuitDesign {
             }
             return false;
         }
-
     }
 
     public void run() {
