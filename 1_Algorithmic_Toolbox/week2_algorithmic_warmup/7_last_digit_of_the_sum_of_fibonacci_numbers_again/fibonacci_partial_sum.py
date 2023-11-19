9ccf901287e69f9ed6f9c733def4711559b41cc7
Nguyen Le <lenguyen_d@hotmail.com>
2023-11-19 08:52:51 -0800

    Complete week2 for Toolbox

--- 1_Algorithmic_Toolbox/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.py
+++ 1_Algorithmic_Toolbox/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.py
@@ -15,8 +15,81 @@ def fibonacci_partial_sum_naive(from_, to):
 
     return _sum % 10
 
+# Implement fast_fib_last_digit by using Pisano period == 60
+def fast_fib_last_digit(n):
+    """
+    Calculates the last digit of the nth Fibonacci number.
+
+    Parameters:
+    n (int): The index of the Fibonacci number to calculate.
+
+    Returns:
+    int: The last digit of the nth Fibonacci number.
+
+    Reference:
+    - Wikipedia: https://en.wikipedia.org/wiki/Fibonacci_number
+
+    Example:
+        >>> fast_fib_last_digit(3)
+        2
+        >>> fast_fib_last_digit(331)
+        9
+        >>> fast_fib_last_digit(327305)
+        5
+    """
+    # Use the formula: F(n) % 10 = F(n % 60) % 10
+    # where F(n) is the nth Fibonacci number
+    # and F(n % 60) is the nth Fibonacci number modulo 60
+    PISANO_PERIOD_10 = 60
+    n = n % PISANO_PERIOD_10
+    if n <= 1:
+        return n
+    else:
+        fib_prev = 0
+        fib_curr = 1
+        for i in range(2, n + 1):
+            fib_prev, fib_curr = fib_curr, (fib_prev + fib_curr) % 10
+        return fib_curr
+
+def fibonacci_partial_sum_fast(from_, to):
+    """
+    Calculates the last digit of the sum of the Fibonacci numbers from the
+    from_th Fibonacci number to the to_th Fibonacci number.
+
+    Parameters:
+    from_ (int): The index of the first Fibonacci number to sum.
+    to (int): The index of the last Fibonacci number to sum.
+
+    Returns:
+    int: The last digit of the sum of the Fibonacci numbers from the
+        from_th Fibonacci number to the to_th Fibonacci number.
+
+    Example:
+        >>> fibonacci_partial_sum_fast(3, 7)
+        1
+        >>> fibonacci_partial_sum_fast(10, 10)
+        5
+        >>> fibonacci_partial_sum_fast(10, 200)
+        2
+    """
+    # Use the formula: F(m) + F(m + 1) + ... + F(n) = F(n + 2) - F(m + 1)
+    # where F(n) is the nth Fibonacci number
+    # and F(m) is the mth Fibonacci number
+    # and F(n + 2) is the (n + 2)th Fibonacci number
+    # and F(m + 1) is the (m + 1)th Fibonacci number
+    # and F(n + 2) - F(m + 1) is the sum of the Fibonacci numbers from the
+    # from_th Fibonacci number to the to_th Fibonacci number
+    # and F(n + 2) - F(m + 1) is the sum of the Fibonacci numbers from the
+    # from_th Fibonacci number to the to_th Fibonacci number modulo 10
+    # and F(n + 2) - F(m + 1) is the last digit of the sum of the Fibonacci
+    # numbers from the from_th Fibonacci number to the to_th Fibonacci number
+    if from_ == to:
+        return fast_fib_last_digit(from_)
+    else:
+        return (fast_fib_last_digit(to + 2) - fast_fib_last_digit(from_ + 1)) % 10
+
 
 if __name__ == '__main__':
     input = sys.stdin.read();
     from_, to = map(int, input.split())
-    print(fibonacci_partial_sum_naive(from_, to))
+    print(fibonacci_partial_sum_fast(from_, to))
--- 1_Algorithmic_Toolbox/week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.py
+++ 1_Algorithmic_Toolbox/week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.py
@@ -15,6 +15,67 @@ def fibonacci_sum_squares_naive(n):
 
     return sum % 10
 
+# Implement fast_fib_last_digit by using Pisano period == 60
+def fast_fib_last_digit(n):
+    """
+    Calculates the last digit of the nth Fibonacci number.
+
+    Parameters:
+    n (int): The index of the Fibonacci number to calculate.
+
+    Returns:
+    int: The last digit of the nth Fibonacci number.
+
+    Reference:
+    - Wikipedia: https://en.wikipedia.org/wiki/Fibonacci_number
+
+    Example:
+        >>> fast_fib_last_digit(3)
+        2
+        >>> fast_fib_last_digit(331)
+        9
+        >>> fast_fib_last_digit(327305)
+        5
+    """
+    # Use the formula: F(n) % 10 = F(n % 60) % 10
+    # where F(n) is the nth Fibonacci number
+    # and F(n % 60) is the nth Fibonacci number modulo 60
+    PISANO_PERIOD_10 = 60
+    n = n % PISANO_PERIOD_10
+    if n <= 1:
+        return n
+    else:
+        fib_prev = 0
+        fib_curr = 1
+        for i in range(2, n + 1):
+            fib_prev, fib_curr = fib_curr, (fib_prev + fib_curr) % 10
+        return fib_curr
+
+# Implement fibonacci_sum_squares_fast(n)
+def fibonacci_sum_squares_fast(n):
+    """
+    Calculates the last digit of the sum of the squares of the first n
+    Fibonacci numbers.
+
+    Parameters:
+    n (int): The number of Fibonacci numbers to sum.
+
+    Returns:
+    int: The last digit of the sum of the squares of the first n Fibonacci
+        numbers.
+
+    Example:
+        >>> fibonacci_sum_squares_fast(7)
+        3
+        >>> fibonacci_sum_squares_fast(73)
+        1
+    """
+    # Use the formula: F(0)^2 + F(1)^2 + ... + F(n)^2 = F(n) * F(n + 1)
+    # where F(n) is the nth Fibonacci number
+    fib_n = fast_fib_last_digit(n)
+    fib_n_plus_1 = fast_fib_last_digit(n + 1)
+    return (fib_n * fib_n_plus_1) % 10
+
 if __name__ == '__main__':
     n = int(stdin.read())
-    print(fibonacci_sum_squares_naive(n))
+    print(fibonacci_sum_squares_fast(n))
