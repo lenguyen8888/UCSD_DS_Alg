d56983a5e3a445900ae23f0955e761389480a880
Nguyen Le <lenguyen_d@hotmail.com>
2023-11-20 22:41:11 -0800

    Passed 1-6 programs in week4. Need to assign Start, Position, End in the proper order

--- 1_Algorithmic_Toolbox/week4_divide_and_conquer/5_number_of_inversions/inversions.py
+++ 1_Algorithmic_Toolbox/week4_divide_and_conquer/5_number_of_inversions/inversions.py
@@ -1,13 +1,55 @@
 import sys
 
 def get_number_of_inversions(a, b, left, right):
+    """
+    Counts the number of inversions in a given array using the merge sort algorithm.
+
+    Parameters:
+    a (list): The input array.
+    b (list): An auxiliary array used for merging.
+    left (int): The starting index of the subarray.
+    right (int): The ending index of the subarray.
+
+    Returns:
+    int: The number of inversions in the array.
+
+    Examples:
+    >>> get_number_of_inversions([2, 3, 9, 2, 9], [0, 0, 0, 0, 0], 0, 5)
+    2
+    >>> get_number_of_inversions([1, 2, 3, 4], [0, 0, 0, 0], 0, 4)
+    0
+    """
     number_of_inversions = 0
     if right - left <= 1:
         return number_of_inversions
     ave = (left + right) // 2
     number_of_inversions += get_number_of_inversions(a, b, left, ave)
     number_of_inversions += get_number_of_inversions(a, b, ave, right)
-    #write your code here
+    # merge a[left:ave] and a[ave:right] to b[left:right] and count inversions
+    i = left
+    j = ave
+    k = left
+    while i < ave and j < right:
+        if a[i] <= a[j]:
+            b[k] = a[i]
+            i += 1
+        else:
+            # a[i] > a[j]
+            b[k] = a[j]
+            j += 1
+            number_of_inversions += ave - i
+        k += 1
+    while i < ave:
+        b[k] = a[i]
+        i += 1
+        k += 1
+    while j < right:
+        b[k] = a[j]
+        j += 1
+        k += 1
+    # copy b[left:right] to a[left:right]
+    for i in range(left, right):
+        a[i] = b[i]
     return number_of_inversions
 
 if __name__ == '__main__':
--- 1_Algorithmic_Toolbox/week4_divide_and_conquer/6_organizing_a_lottery/points_and_segments.py
+++ 1_Algorithmic_Toolbox/week4_divide_and_conquer/6_organizing_a_lottery/points_and_segments.py
@@ -1,10 +1,62 @@
 # Uses python3
 import sys
+DEBUG = False
 
 def fast_count_segments(starts, ends, points):
+    """
+    Counts the number of segments that contain each point in a given list of points.
+
+    Args:
+        starts (list): List of starting points of segments.
+        ends (list): List of ending points of segments.
+        points (list): List of points to count.
+
+    Returns:
+        list: List of counts, where each count represents the number of segments that contain the corresponding point.
+
+    Examples:
+        >>> fast_count_segments([0, 7], [5, 10], [1, 6, 11])
+        [1, 0, 0]
+        >>> fast_count_segments([0, -3, 7], [5, 2, 10], [1, 6])
+        [2, 0]
+
+    """
     cnt = [0] * len(points)
-    #write your code here
+    points_type = []
+    # 0: start point, 1: position, 2: end point
+    for i in range(len(starts)):
+        points_type.append((starts[i], 0, i))
+    for i in range(len(points)):
+        # the third element of the point is the index of 
+        # the point in the points list to save time when
+        # assigning the count of the point to the cnt list
+        points_type.append((points[i], 1, i))
+    for i in range(len(ends)):
+        points_type.append((ends[i], 2, i))
+    
+    # sort the points_type list by the first element of each point and then by the second element
+    points_type.sort(key=lambda x: (x[0], x[1]))
+  
+    # count segments
+    segments = 0
+
+    # iterate over the points_type list using the range of its length
+    for i in range(len(points_type)):
+        if points_type[i][1] == 0:
+            # if the second element of the current point is 0, it is a start point
+            # increment the segments count
+            segments += 1
+        elif points_type[i][1] == 2:
+            # if the second element of the current point is 1, it is an end point
+            # decrement the segments count
+            segments -= 1
+        else:
+            # if the second element of the current point is 2, it is a point
+            # set the count of the point to the current segments count
+            cnt[points_type[i][2]] = segments
     return cnt
+
+    
 
 def naive_count_segments(starts, ends, points):
     cnt = [0] * len(points)
@@ -13,8 +65,16 @@ def naive_count_segments(starts, ends, points):
             if starts[j] <= points[i] <= ends[j]:
                 cnt[i] += 1
     return cnt
+# write test_cases
+def test_cases():
+    assert fast_count_segments([0, 7], [5, 10], [1, 6, 11]) == [1, 0, 0]
+    assert fast_count_segments([0, -3, 7], [5, 2, 10], [1, 6]) == [2, 0]
+    print("Test passed...")
 
 if __name__ == '__main__':
+    if DEBUG:
+        test_cases()
+
     input = sys.stdin.read()
     data = list(map(int, input.split()))
     n = data[0]
@@ -23,6 +83,6 @@ if __name__ == '__main__':
     ends   = data[3:2 * n + 2:2]
     points = data[2 * n + 2:]
     #use fast_count_segments
-    cnt = naive_count_segments(starts, ends, points)
+    cnt = fast_count_segments(starts, ends, points)
     for x in cnt:
         print(x, end=' ')
