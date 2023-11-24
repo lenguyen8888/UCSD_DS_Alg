cc190af514637c3fc59408b2206cd33389b868a9
Nguyen Le <lenguyen_d@hotmail.com>
2023-11-24 15:33:51 -0800

    Passed week6 in Algorithm Toolbox

--- 1_Algorithmic_Toolbox/week6_dynamic_programming2/2_partitioning_souvenirs/partition3.py
+++ 1_Algorithmic_Toolbox/week6_dynamic_programming2/2_partitioning_souvenirs/partition3.py
@@ -1,20 +1,123 @@
 # Uses python3
 import sys
 import itertools
+DEBUG = False
 
-def partition3(A):
-    for c in itertools.product(range(3), repeat=len(A)):
-        sums = [None] * 3
-        for i in range(3):
-            sums[i] = sum(A[k] for k in range(len(A)) if c[k] == i)
+def partition3(arr):
+    possible, dp = partition3_dp(arr)
+    return 1 if possible else 0
 
-        if sums[0] == sums[1] and sums[1] == sums[2]:
-            return 1
+def partition3_dp(arr):
+    total_sum = sum(arr)
+    n = len(arr)
 
-    return 0
+    if total_sum % 3 != 0:
+        return False, None
+
+    dp = [[[False for _ in range(total_sum + 1)] for _ in range(total_sum + 1)] for _ in range(n + 1)]
+    dp[0][0][0] = True
+
+    target = total_sum // 3
+
+    for i in range(1, n + 1):
+        for j in range(target + 1):
+            for k in range(target + 1):
+                dp[i][j][k] = dp[i - 1][j][k]
+                if j >= arr[i - 1]:
+                    dp[i][j][k] = dp[i][j][k] or dp[i - 1][j - arr[i - 1]][k]
+                if k >= arr[i - 1]:
+                    dp[i][j][k] = dp[i][j][k] or dp[i - 1][j][k - arr[i - 1]]
+
+    return dp[n][target][target], dp
+
+def partition_back(arr):
+    is_possible, dp = partition3_dp(arr)
+
+    if not is_possible:
+        return "No partition possible"
+
+    n = len(arr)
+    total_sum = sum(arr)
+    target = total_sum // 3
+
+    group1 = []
+    group2 = []
+    group3 = []
+
+    i, j, k = n, target, target
+
+    while i > 0: # and (j > 0 or k > 0):
+        if j >= arr[i - 1] and dp[i - 1][j - arr[i - 1]][k]:
+            group1.append(arr[i - 1])
+            j -= arr[i - 1]
+        elif k >= arr[i - 1] and dp[i - 1][j][k - arr[i - 1]]:
+            group2.append(arr[i - 1])
+            k -= arr[i - 1]
+        else:
+            group3.append(arr[i - 1])
+        i -= 1
+
+    return group1, group2, group3
+
+
+import random
+
+def generate_list_with_sum(length, total_sum):
+    """ Generate a list of `length` integers that add up to `total_sum` """
+    if length == 1:
+        return [total_sum]
+    # Generate a list of length - 1 integers between 1 and (total_sum - 1) // length
+    nums = [random.randint(1, (total_sum - 1) // length) for _ in range(length - 1)]
+    # Add the last number to make the total sum equal to `total_sum`
+    nums.append(total_sum - sum(nums))
+    random.shuffle(nums)
+    return nums
+
+def generate_random_div_by3_list(max_length=20, value_range=(1, 30)):
+    # Ensure the length is a multiple of 3 for equal partitioning
+    length = random.randint(1, max_length // 3) * 3
+    third_length = length // 3
+
+    # Generate three lists with equal sum
+    third_sum = random.randint(value_range[0] * third_length, value_range[1] * third_length)
+    list1 = generate_list_with_sum(third_length, third_sum)
+    list2 = generate_list_with_sum(third_length, third_sum)
+    list3 = generate_list_with_sum(third_length, third_sum)
+
+    combined_list = list1 + list2 + list3
+    random.shuffle(combined_list)
+    return combined_list
+
+def generate_list_not_div_by3(max_length=20, value_range=(1, 30)):
+    nums = generate_random_div_by3_list(max_length, value_range)
+    max_val = sum(nums)
+    # add max_val + 1 and max_val + 2 to make the sum divisible by 3
+    # and the group since these 2 values will be in different groups
+    # We can divide the list into 3 groups that have equal sum
+    nums.append(max_val + 1)
+    nums.append(max_val + 2)
+    return nums
+
+def test_cases():
+    # generate 10 random list that can be divided into 3 equal sum subsets
+    for good_id in range(10):
+        nums = generate_random_div_by3_list()
+        assert(partition3(nums) == 1)
+        print(good_id, nums)
+        possible, dp = partition3_dp(nums)
+        assert(possible)
+        print(partition_back(nums))
+    for bad_id in range(10):
+        nums = generate_list_not_div_by3()
+        print(bad_id, nums)
+        assert(partition3(nums) == 0)
+    print("Test passed")
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n, *A = list(map(int, input.split()))
-    print(partition3(A))
+    if DEBUG:
+        test_cases()
+    else:
+        input = sys.stdin.read()
+        n, *A = list(map(int, input.split()))
+        print(partition3(A))
 
--- 1_Algorithmic_Toolbox/week6_dynamic_programming2/3_maximum_value_of_an_arithmetic_expression/placing_parentheses.py
+++ 1_Algorithmic_Toolbox/week6_dynamic_programming2/3_maximum_value_of_an_arithmetic_expression/placing_parentheses.py
@@ -1,4 +1,6 @@
 # Uses python3
+DEBUG = False
+
 def evalt(a, b, op):
     if op == '+':
         return a + b
@@ -8,11 +10,98 @@ def evalt(a, b, op):
         return a * b
     else:
         assert False
+# write a function that finds the minimum and maximum values of an arithmetic expression
+def min_and_max(i, j, min_table, max_table, operators):
+    """
+    Finds the minimum and maximum values of an arithmetic expression.
+
+    Args:
+        i (int): The starting index of the arithmetic expression.
+        j (int): The ending index of the arithmetic expression.
+        min_table (list): The dp table for the minimum values.
+        max_table (list): The dp table for the maximum values.
+        operators (list): The list of operators in the arithmetic expression.
+
+    Returns:
+        tuple: The minimum and maximum values of the arithmetic expression.
+
+    Examples:
+        >>> min_and_max(1, 3,
+         [[0, 0, 0, 0], [0, 0, 0, 0]
+        , [0, 0, 3, -4], [0, 0, 0, 0]]
+
+        , [[0, 0, 0, 0], [0, 0, 0, 0]
+        , [0, 0, -4, 3], [0, 0, 0, 0]]
+
+        , ['+', '-', '*']
+        )
+        (-4, 3)
+    """
+    # initialize the minimum and maximum values
+    min_value = float('inf')
+    max_value = float('-inf')
+    # iterate through the range of i to j
+    for k in range(i, j):
+        # a = evalt(max_table[i][k], max_table[k + 1][j], operators[k])
+        a = evalt(max_table[i][k], max_table[k + 1][j], operators[k])
+        # b = evalt(max_table[i][k], min_table[k + 1][j], operators[k])
+        b = evalt(max_table[i][k], min_table[k + 1][j], operators[k])
+        # c = evalt(min_table[i][k], max_table[k + 1][j], operators[k])
+        c = evalt(min_table[i][k], max_table[k + 1][j], operators[k])
+        # d = evalt(min_table[i][k], min_table[k + 1][j], operators[k])
+        d = evalt(min_table[i][k], min_table[k + 1][j], operators[k])
+        # min_value = min(min_value, a, b, c, d)
+        min_value = min(min_value, a, b, c, d)
+        # max_value = max(max_value
+        max_value = max(max_value, a, b, c, d)
+    # return the minimum and maximum values
+    return min_value, max_value
 
 def get_maximum_value(dataset):
-    #write your code here
-    return 0
+    """
+    Calculates the maximum value of an arithmetic expression.
+
+    Args:
+        dataset (str): The arithmetic expression in the form of a string.
+
+    Returns:
+        int: The maximum value of the arithmetic expression.
+
+    Examples:
+        >>> get_maximum_value('1+5')
+        6
+        >>> get_maximum_value('5-8+7*4-8+9')
+        200
+    """
+    # using dynamic programming to find the maximum value of an arithmetic expression
+    # of the form: a1 op1 a2 op2 a3 op3 ... an-1 opn an
+    # split the string into a list of integers and operators
+    integers = [int(x) for x in dataset if x.isdigit()]
+    operators = [x for x in dataset if not x.isdigit()]
+    # initialize the dp tables
+    min_table = [[0] * len(integers) for _ in range(len(integers))]
+    max_table = [[0] * len(integers) for _ in range(len(integers))]
+    # set all the diagonal values to the integers
+    for i in range(len(integers)):
+        min_table[i][i] = integers[i]
+        max_table[i][i] = integers[i]
+    # iterate through the dp tables
+    for s in range(1, len(integers)):
+        for i in range(len(integers) - s):
+            j = i + s
+            min_table[i][j], max_table[i][j] = min_and_max(i, j, min_table, max_table, operators)
+    # return the top right corner of the dp table
+    return max_table[0][len(integers) - 1]
+
+
+# write test cases
+def test_cases():
+    assert get_maximum_value('1+5') == 6
+    assert get_maximum_value('5-8+7*4-8+9') == 200
+    print('All test cases passed.')
 
 
 if __name__ == "__main__":
+    if DEBUG:
+        test_cases()
     print(get_maximum_value(input()))
