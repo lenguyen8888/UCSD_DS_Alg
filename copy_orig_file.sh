a524c17b906aca70ef81cff394b217766b59376f
Nguyen Le <lenguyen_d@hotmail.com>
2023-11-24 17:08:23 -0800

    Update class 1 Algorithm Toolbox orig source files

--- copy_orig_file.sh
+++ copy_orig_file.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-# write a for loop to use find and copy sample.(java|py|cpp) to sample.(java|py|cpp).orig.(java|py|cpp)
-for ext in py cpp java; do
-    echo "Copying $ext files"
-    find . -type f -name "*.$ext" -exec cp {} {}.orig.$ext \;
+# Directory where to search files. Replace with the desired path.
+SEARCH_DIR="."
+
+# Use find to locate .py, .java, and .cpp files and loop through them
+find "$SEARCH_DIR" -type f \( -name "*.py" -o -name "*.java" -o -name "*.cpp" \) | while read file; do
+    # Determine the new file name by adding .orig before the file extension
+    new_file="${file%.*}.orig.${file##*.}"
+    echo "Done with $file"
+    # Copy the original file to the new file
+    cp "$file" "$new_file"
 done
+
+echo "Files have been copied."
+
