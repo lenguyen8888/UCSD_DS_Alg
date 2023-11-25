212543c3e24675163ad0eb0a71408f528e7c2f8a
Nguyen Le <lenguyen_d@hotmail.com>
2023-11-24 16:09:08 -0800

    Extensive random test to check parition3 logic pays off. Program passed automatic checking

--- 1_Algorithmic_Toolbox/week6_dynamic_programming2/2_partitioning_souvenirs/partition3.py
+++ 1_Algorithmic_Toolbox/week6_dynamic_programming2/2_partitioning_souvenirs/partition3.py
@@ -1,13 +1,40 @@
 # Uses python3
 import sys
 import itertools
+
+# Set to True to run test cases
 DEBUG = False
 
 def partition3(arr):
+    """
+    Determines if it is possible to partition the given array into three subsets with equal sum.
+    This is a wrapper function that calls partition3_dp and returns 1 if partitioning is possible
+
+    Args:
+        arr (list): The input array.
+
+    Returns:
+        int: Returns 1 if it is possible to partition the array into three subsets with equal sum, 0 otherwise.
+    """
+    # See partition3_dp for details
     possible, dp = partition3_dp(arr)
     return 1 if possible else 0
 
 def partition3_dp(arr):
+    """
+    Determines if it is possible to partition the given array into three subsets with equal sums.
+
+    Args:
+        arr (list): The input array of integers.
+
+    Returns:
+        tuple: A tuple containing a boolean value indicating if partitioning is possible and the
+         dynamic programming table.
+        
+    The extra dynamic programming table is returned for debugging purposes.
+
+    """
+
     total_sum = sum(arr)
     n = len(arr)
 
@@ -17,20 +44,48 @@ def partition3_dp(arr):
     dp = [[[False for _ in range(total_sum + 1)] for _ in range(total_sum + 1)] for _ in range(n + 1)]
     dp[0][0][0] = True
 
+    # target is the sum of each subset
     target = total_sum // 3
 
+    # i entries in the dp table represent the first i elements of the array
+    # j entries represent the sum of the first subset
+    # k entries represent the sum of the second subset
+    # dp[i][j][k] is True if it is possible to partition the first i elements of the array into
+    # three subsets with sums j, k, and total_sum - j - k respectively
     for i in range(1, n + 1):
         for j in range(target + 1):
             for k in range(target + 1):
+                # If it is possible to partition the first i - 1 elements of the array into three
                 dp[i][j][k] = dp[i - 1][j][k]
+
+                # If j >= arr[i - 1], then it is possible to partition the first i elements of the
                 if j >= arr[i - 1]:
+                    # If it is possible to partition the first i - 1 elements of the array into
+                    # three subsets with sums j - arr[i - 1], k, and total_sum - j - k respectively
                     dp[i][j][k] = dp[i][j][k] or dp[i - 1][j - arr[i - 1]][k]
+
+                # similarly, if k >= arr[i - 1], then it is possible to partition the first i
                 if k >= arr[i - 1]:
+                    # If it is possible to partition the first i - 1 elements of the array into
+                    # three subsets with sums j, k - arr[i - 1], and total_sum - j - k respectively
                     dp[i][j][k] = dp[i][j][k] or dp[i - 1][j][k - arr[i - 1]]
 
     return dp[n][target][target], dp
 
 def partition_back(arr):
+    """
+    Partitions the given array into three groups such that the sum of elements in each group is equal.
+
+    Args:
+        arr (list): The input array.
+
+    Returns:
+        tuple: A tuple containing three lists representing the three groups after partitioning.
+
+    Note:
+        This function assumes that partitioning is possible. Use partition3 to check if partitioning is possible.
+        Useful for debugging.
+    """
     is_possible, dp = partition3_dp(arr)
 
     if not is_possible:
@@ -46,14 +101,24 @@ def partition_back(arr):
 
     i, j, k = n, target, target
 
+    # iterate backwards through the dp table to find the
+    # elements that were used to partition the array
     while i > 0: # and (j > 0 or k > 0):
         if j >= arr[i - 1] and dp[i - 1][j - arr[i - 1]][k]:
+            # Found that the element at index i - 1 was used to partition the array
+            # to group1 (j - arr[i - 1], k, total_sum - j - k)
+            # add the element to group1 and subtract it from j
             group1.append(arr[i - 1])
             j -= arr[i - 1]
         elif k >= arr[i - 1] and dp[i - 1][j][k - arr[i - 1]]:
+            # Found that the element at index i - 1 was used to partition the array
+            # to group2 (j, k - arr[i - 1], total_sum - j - k)
+            # add the element to group2 and subtract it from k
             group2.append(arr[i - 1])
             k -= arr[i - 1]
         else:
+            # The rest of the elements were used to partition the array
+            # to group3 (j, k, total_sum - j - k)
             group3.append(arr[i - 1])
         i -= 1
 
@@ -99,15 +164,17 @@ def generate_list_not_div_by3(max_length=20, value_range=(1, 30)):
     return nums
 
 def test_cases():
+    NUM_TESTS = 10
     # generate 10 random list that can be divided into 3 equal sum subsets
-    for good_id in range(10):
+    for good_id in range(NUM_TESTS):
         nums = generate_random_div_by3_list()
         assert(partition3(nums) == 1)
         print(good_id, nums)
         possible, dp = partition3_dp(nums)
         assert(possible)
         print(partition_back(nums))
-    for bad_id in range(10):
+
+    for bad_id in range(NUM_TESTS):
         nums = generate_list_not_div_by3()
         print(bad_id, nums)
         assert(partition3(nums) == 0)
